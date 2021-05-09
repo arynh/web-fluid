d56b10a0914ae4d4682ce8340d4bea9192336678
Rishi <rishipandey125@gmail.com>
2021-05-09 14:52:27 -0500

    modifying RHS (divergence) to account for solid velocities

--- src/simulation/negative-divergence.js
+++ src/simulation/negative-divergence.js
@@ -23,11 +23,35 @@ export const createNegativeDivergenceKernel = (gpu, nx, ny, nz, cellSize) =>
           velocityZ[i][j][k + 1] -
           velocityZ[i][j][k]);
 
-      // TODO: add pseudocode 5.4 to adjust RHS for boundary conditions
+
+      // modifying RHS (divergence) to account for solid velocities
+      if (voxelStates[i-1][j][k] === SOLID) {
+        divergence -= scale * velocityX[i][j][k];
+      }
+      if (voxelStates[i+1][j][k] === SOLID) {
+        divergence += scale * velocityX[i+1][j][k];
+      }
+
+      if (voxelStates[i][j-1][k] === SOLID) {
+        divergence -= scale * velocityY[i][j][k];
+      }
+      if (voxelStates[i][j+1][k] === SOLID) {
+        divergence += scale * velocityY[i][j+1][k];
+      }
+
+      if (voxelStates[i][j][k-1] === SOLID) {
+        divergence -= scale * velocityZ[i][j][k];
+      }
+
+      if (voxelStates[i][j][k+1] === SOLID) {
+        divergence += scale * velocityZ[i][j][k+1];
+      }
+      return divergence;
     })
     .setTactic("precision") // vector math should be high precision
     .setConstants({
       CELL_SIZE: cellSize,
       FLUID: STATE_ENUM.FLUID,
+      SOLID: STATE_ENUM.SOLID
     })
     .setOutput([nx, ny, nz]);
